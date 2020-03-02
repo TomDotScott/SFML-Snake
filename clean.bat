6bd950db2f31e79d3ce9a0a77e8cb44d226322ec
Tom Scott <48756858+TomDotScott@users.noreply.github.com>
2020-03-02 17:09:15 +0000

    Removed

--- clean.bat
+++
@@ -1,9 +0,0 @@
-del /s /q x64\Release\*.*
-del /s /q x64\Debug\*.*
-del /s /q .vs\*.*
-del /s /q SnakeGame\x64\Release\*.*
-del /s /q SnakeGame\x64\Debug\*.*
-
-rd /s /q x64
-rd /s /q .vs
-rd /s /q SnakeGame\x64
