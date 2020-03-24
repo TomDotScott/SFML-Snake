4cbaabb37768e0f12a96f25d3fa24084d3caaad8
tomdotscott <tom.scott45456@gmail.com>
2020-03-24 17:21:48 +0000

    Constructor and Destructor

--- SnakeGame/EventManager.cpp
+++ SnakeGame/EventManager.cpp
@@ -1,1 +1,15 @@
 #include "EventManager.h"
+EventManager::EventManager() : m_hasFocus(true)
+{
+	LoadBindings();
+}
+
+EventManager::~EventManager()
+{
+	for(auto& itr : m_bindings)
+	{
+		delete itr.second;
+		itr.second = nullptr;
+	}
+}
+
