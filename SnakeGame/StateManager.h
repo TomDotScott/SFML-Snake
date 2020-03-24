2f75252103f475fa23a13a6352e437c0695d0626
tomdotscott <tom.scott45456@gmail.com>
2020-03-24 15:13:25 +0000

    StateManager Header
    All fo the functions and methods that the StateManager will have were outlined in the header file

--- SnakeGame/StateManager.h
+++ SnakeGame/StateManager.h
@@ -28,6 +28,35 @@ using StateFactory = std::unordered_map<StateType, std::function<BaseState* (voi
 class StateManager
 {
 public:
+	StateManager(SharedContext* _shared);
+	~StateManager();
 
+	void Update();
+	void Render();
+
+	void ProcessRequests();
+
+	SharedContext* GetContext();
+	bool HasState(const StateType& _type);
+
+	//Changes the current state to whatever is passed in to the function
+	void SwitchTo(const StateType& _type);
+
+	//Removes a state from the state stack
+	void Remove(const StateType& _type);
+private:
+	//Methods
+	void CreateState(const StateType& _type);
+	void RemoveState(const StateType& _type);
+
+	template<class T>;
+	void RegisterState(const StateType& _type);
+
+	//Members
+	SharedContext* m_shared;
+	StateContainer m_states;
+	//We only want to remove states once they have been finished with
+	TypeContainer m_toRemove;
+	StateFactory m_stateFactory;
 	
 };
